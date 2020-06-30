5050cdf463ce582d75ce277bd94c3acb6f2684f0
Ahmed199764 <runner@fv-az71.ij34b24smqvezbholevadjsfbc.bx.internal.cloudapp.net>
2020-06-30 18:43:17 +0000

    Updates

---
+++ precache-manifest.6ba4c95361661a08081c19773d6525aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a41f804ab12a1393f2afd4ecd5a8893",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/index.html"
+  },
+  {
+    "revision": "2db3a5423a6b4d9777d9",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/static/css/main.cd05a6b9.chunk.css"
+  },
+  {
+    "revision": "3c3fe42eb5bed02bd8ed",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/static/js/2.62d548b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/static/js/2.62d548b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db3a5423a6b4d9777d9",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/static/js/main.637477d0.chunk.js"
+  },
+  {
+    "revision": "b53466456f699afccc85",
+    "url": "/AhmedAbuSamaan.github.io/restyNew/static/js/runtime-main.4162adf5.js"
+  }
+]);
