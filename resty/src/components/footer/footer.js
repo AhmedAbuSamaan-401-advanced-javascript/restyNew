9a12e6552aced0fd23133e4437a2419441367252
Ahmed199764 <Ahmed199764@hotmail.com>
2020-06-30 14:52:03 +0300

    test

---
+++ resty/src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.scss';
+
+const Footer = () => {
+    return (
+      <footer>
+        <h1>@ 2020 Code Fellows</h1>
+      </footer>
+    );
+  };
+
+export default Footer;
